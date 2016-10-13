e29f6fe432260aa1cdb2d412cb7a84b4b21f4164
Andy White <andy.white@digital.justice.gov.uk>
2016-10-13 14:04:24 +0100

    Use DATABASE_URL when set

--- lib/mediators.rb
+++ lib/mediators.rb
@@ -1,3 +1,4 @@
+require 'logger'
 require 'grape'
 require 'grape-entity'
 require 'otr-activerecord'
@@ -6,4 +7,13 @@ require_relative 'mediators/models/mediator'
 require_relative 'mediators/entities/mediator'
 require_relative 'mediators/api'
 
-OTR::ActiveRecord.configure_from_file! File.expand_path("../../config/database.yml", __FILE__)
+LOGGER = Logger.new(STDOUT)
+LOGGER.level = Logger::DEBUG
+
+if ENV['DATABASE_URL']
+  LOGGER.info "Configuring DB connection from DATABASE_URL env var"
+  OTR::ActiveRecord.configure_from_url! ENV['DATABASE_URL']
+else
+  LOGGER.info "Configuring DB connection from database.yml"
+  OTR::ActiveRecord.configure_from_file! File.expand_path("../../config/database.yml", __FILE__)
+end
