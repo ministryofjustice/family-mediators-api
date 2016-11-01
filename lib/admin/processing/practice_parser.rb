98f4a0ce1de6725a9e66c72fcb3916410a0522f6
Andy White <andy.white@digital.justice.gov.uk>
2016-11-01 10:11:52 +0000

    Fix URL and tel recognition

--- lib/admin/processing/practice_parser.rb
+++ lib/admin/processing/practice_parser.rb
@@ -1,25 +1,27 @@
 module Admin
   module Processing
     class PracticeParser
+      RECORD_SEPARATOR = "\n"
+      PART_SEPARATOR = '|'
       EMAIL_REGEX = /([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})/i
-      TEL_REGEX = /\d{5,12}/
+      TEL_REGEX = /[\d\s-]{5,12}/
       POSTCODE_REGEX = /((GIR\s*0AA)|((([A-PR-UWYZ][0-9]{1,2})|(([A-PR-UWYZ][A-HK-Y][0-9]{1,2})|(([A-PR-UWYZ][0-9][A-HJKSTUW])|([A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRVWXY]))))\s*[0-9][ABD-HJLNP-UW-Z]{2}))/i
       URL_REGEX = /\A((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?\Z/i
 
       def self.parse(data)
-        data.split("\n").map { |practice| parse_address(practice) }
+        data.split(RECORD_SEPARATOR).map { |practice| parse_address(practice) }
       end
 
       # private
 
       def self.parse_address(practice)
-        elements = practice.split(':').map { |element| sanitise(element) }
+        parts = practice.split(PART_SEPARATOR).map { |part| sanitise(part) }
 
         {
-          "address" => find(elements, POSTCODE_REGEX),
-          "tel" => find(elements, TEL_REGEX),
-          "email" => find(elements, EMAIL_REGEX),
-          "url" => find(elements, URL_REGEX)
+          "address" => find(parts, POSTCODE_REGEX),
+          "tel" => find(parts, TEL_REGEX),
+          "email" => find(parts, EMAIL_REGEX),
+          "url" => find(parts, URL_REGEX)
         }
       end
 
@@ -27,8 +29,8 @@ module Admin
         string.nil? ? nil : string.strip.squeeze(' ')
       end
 
-      def self.find(elements, regex)
-        elements.each { |element| return element if element =~ regex }
+      def self.find(parts, regex)
+        parts.each { |part| return parts.delete(part) if part =~ regex }
         nil
       end
     end
