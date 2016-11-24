9661fdf47cb9c86d35334ecb55bf62d89c3e5f75
Nick Rowlands <nick@rowlando.com>
2016-11-24 13:52:49 +0000

    Workaround hanami custom predicate error when in nested schema

--- lib/admin/validators/practice_validator.rb
+++ lib/admin/validators/practice_validator.rb
@@ -1,11 +1,9 @@
 class PracticeValidator
   include Hanami::Validations
 
-  predicate :phone_number?, message: 'must be UK phone number' do |current|
-    current.match(/^(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?$/)
-  end
+  UK_PHONE_FORMAT = /^(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?$/
 
   validations do
-    required('tel') { none? | phone_number? }
+    required('tel') { none? | format?(UK_PHONE_FORMAT) }
   end
 end
