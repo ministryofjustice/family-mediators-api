517a394bcc75b331a96e4567ed7627c8d10c75bd
Nick Rowlands <nick@rowlando.com>
2016-11-28 12:20:02 +0000

    Remove telephone_number? error message because it's not used

--- lib/admin/validators/practice_validator.rb
+++ lib/admin/validators/practice_validator.rb
@@ -6,7 +6,7 @@ module Admin
       UK_PHONE_FORMAT = /^(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?$/
       HTTP_FORMAT = URI.regexp(%w(http https))
 
-      predicate(:telephone_number?, message: 'BLAHDHD') do |current|
+      predicate(:telephone_number?) do |current|
         current.nil? || current.match(UK_PHONE_FORMAT)
       end
 
