2efccc9901d1e27bf67466589c0bbb565277633b
Nick Rowlands <nick@rowlando.com>
2016-12-05 12:25:27 +0000

    Add email validation and loosen email parse regex so more potential email addresses get through to validation stage

--- support/factories/practice_hash.rb
+++ support/factories/practice_hash.rb
@@ -5,6 +5,15 @@ FactoryGirl.define do
     address '15 Smith Street, London WC1R 4RL'
     tel nil
     url nil
+    email nil
+
+    trait :valid_email do
+      email 'valid@email.com'
+    end
+
+    trait :invalid_email do
+      email 'invalidemail.com'
+    end
 
     initialize_with { attributes }
   end
