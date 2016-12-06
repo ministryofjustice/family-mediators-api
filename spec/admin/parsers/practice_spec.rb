45b6d53cacccdb337325c0486a8db79339c883d9
Nick Rowlands <nick@rowlando.com>
2016-12-06 16:00:14 +0000

    Add extra test cases to refactored practice_spec

--- spec/admin/parsers/practice_spec.rb
+++ spec/admin/parsers/practice_spec.rb
@@ -17,14 +17,21 @@ module Admin
       context 'URL_REGEX' do
         %w{ http://foo.com https://foo.com foo.com http://www.bar.co.uk foo.com/a/path/ http://foo.com/a/path/}.each do |url|
           it "matches URL of the form: #{url}" do
-            expect(url =~ Practice::URL_REGEX).to_not be_nil
-          end
-
-          it 'does not match an email-like string' do
-            expect('andy@foo.com' =~ Practice::URL_REGEX).to be_nil
+            expect(url).to match(Practice::URL_REGEX)
           end
         end
 
+        it 'does not match an email-like string' do
+          expect('andy@foo.com').to_not match(Practice::URL_REGEX)
+        end
+      end
+
+      context 'TEL_REGEX' do
+        [ '07974 877182', '0201 308 2097', '0300 4000 636', '123456789', '07977-789786' ].each do |tel|
+          it "matches telephone number of the form: #{tel}" do
+            expect(tel).to match(Practice::TEL_REGEX)
+          end
+        end
       end
 
       context 'single unparsed practice' do
@@ -71,91 +78,58 @@ module Admin
         end
 
         context 'when all parts present' do
-          let(:unparsed_practice) { create(:unparsed_practice, :tel, :email, :url)}
-          let(:parsed_practice) { create(:parsed_practice, :tel, :email, :url) }
+          let(:unparsed_practice) { create(:unparsed_practice, :all_parts)}
+          let(:parsed_practice) { create(:parsed_practice, :all_parts) }
           it { should include(address: parsed_practice[:address])}
           it { should include(tel: parsed_practice[:tel])}
           it { should include(email: parsed_practice[:email])}
           it { should include(url: parsed_practice[:url])}
         end
 
+        context 'when parts are in different order' do
+          let(:unparsed_practice) { create(:shuffled_unparsed_practice, :all_parts)}
+          let(:parsed_practice) { create(:parsed_practice, :all_parts) }
+          it { should include(address: parsed_practice[:address])}
+          it { should include(tel: parsed_practice[:tel])}
+          it { should include(email: parsed_practice[:email])}
+          it { should include(url: parsed_practice[:url])}
+        end
+
+        context 'when parts have whitespace' do
+          let(:unparsed_practice) { create(:whitespaced_unparsed_practice, :all_parts)}
+          let(:parsed_practice) { create(:parsed_practice, :all_parts) }
+          it 'should remove excess whitespace for address' do
+            expect(subject).to include(address: parsed_practice[:address])
+          end
+
+          it 'should remove excess whitespace for tel' do
+            expect(subject).to include(tel: parsed_practice[:tel])
+          end
+
+          it 'should remove excess whitespace for email' do
+            expect(subject).to include(email: parsed_practice[:email])
+          end
+
+          it 'should remove excess whitespace for url' do
+            expect(subject).to include(url: parsed_practice[:url])
+          end
+        end
+
+
       end
 
+      context 'multiple practices' do
+        subject { Practice.parse(unparsed_practices) }
+        let(:unparsed_practices) do
+          "#{create(:unparsed_practice)}
+          #{create(:unparsed_practice)}
+          #{create(:unparsed_practice)}"
+        end
 
-      # describe('#parse') do
-      #
-      #   context 'URL recognition' do
-      #     it 'Does not match an email' do
-      #       expect('andy@foo.com' =~ Practice::URL_REGEX).to be_nil
-      #     end
-      #
-      #     %w{ http://foo.com https://foo.com foo.com http://www.bar.co.uk foo.com/a/path/ http://foo.com/a/path/}.each do |url|
-      #       it "Matches URL of the form: #{url}" do
-      #         expect(url =~ Practice::URL_REGEX).to_not be_nil
-      #       end
-      #     end
-      #   end
-      #
-      #   context 'Tel. number recognition' do
-      #     [ '07974 877182', '0201 308 2097', '0300 4000 636', '123456789', '07977-789786' ].each do |tel|
-      #       it "Matches tel. number of the form: #{tel}" do
-      #         expect(tel =~ Practice::TEL_REGEX).to_not be_nil
-      #       end
-      #     end
-      #   end
-      #
-      #   context 'practice with address, tel, email and url parts' do
-      #     subject { Practice.parse(practice).first }
-      #
-      #     it { should include(:address => '1 Null Way, Wessex CM2 9AF')}
-      #     it { should include(:tel => '01245 605040')}
-      #     it { should include(:email => 'foo@bar.com')}
-      #     it { should include(:url => 'http://www.foobar.com/baz/')}
-      #   end
-      #
-      #   context 'practice with address only' do
-      #     subject { Practice.parse(address_only_practice).first }
-      #
-      #     it { should include(:address => '1 Null Way, Wessex CM2 9AF')}
-      #     it { should include(:tel => nil)}
-      #     it { should include(:email => nil)}
-      #   end
-      #
-      #   context 'multiple practices' do
-      #     subject { Practice.parse(multiple_practices) }
-      #
-      #     it 'returns an array of parsed practice hashes' do
-      #       expect(subject.size).to eq(4)
-      #     end
-      #   end
-      #
-      #   context 'trims and compresses multiple spaces to one space for all fields' do
-      #     subject { Practice.parse(practice_with_undesirable_spaces).first }
-      #
-      #     it { should include(:address => '1 Null Way, Wessex CM2 9AF')}
-      #     it { should include(:tel => '01245 605040')}
-      #     it { should include(:email => 'foo@bar.com')}
-      #   end
-      #
-      #   context 'missing trailing colons' do
-      #     let(:practice_without_trailing_colon) { '1 Null Way, Wessex CM2 9AF| 01245 605040 |foo@bar.com|http://www.foobar.com/baz/'}
-      #
-      #     subject { Practice.parse(practice_without_trailing_colon).first }
-      #
-      #     it 'should parse' do
-      #       expect(subject.size).to eq(4)
-      #     end
-      #
-      #   end
-      #
-      #   context 'handles practice parts in any order' do
-      #     subject { Practice.parse(reordered_practice_parts).first }
-      #
-      #     it { should include(:address => '1 Null Way, Wessex CM2 9AF')}
-      #     it { should include(:tel => '01245 605040')}
-      #     it { should include(:email => 'foo@bar.com')}
-      #   end
-      # end
+        it 'returns an array of parsed practice hashes' do
+          expect(subject.size).to eq(3)
+        end
+      end
     end
   end
 end
--- support/factories/practice.rb
+++ support/factories/practice.rb
@@ -6,6 +6,7 @@ FactoryGirl.define do
     tel nil
     url nil
     email nil
+    # shuffle false
 
     trait :email do
       email 'valid@email.com'
@@ -27,10 +28,29 @@ FactoryGirl.define do
       address '15 Smith Street, London'
     end
 
+    trait :all_parts do
+      url
+      tel
+      email
+    end
+
+    # trait :shuffle_parts do
+    #   shuffle true
+    # end
+
     initialize_with { new("#{[address, tel, url, email].compact.join('|')}") }
 
     factory :parsed_practice do
       initialize_with { attributes }
     end
+
+    factory :shuffled_unparsed_practice do
+      initialize_with { new("#{[address, tel, url, email].shuffle.compact.join('|')}") }
+    end
+
+    factory :whitespaced_unparsed_practice do
+      initialize_with { new("#{[address, tel, url, email].compact.join('  |  ')}") }
+    end
+
   end
 end
