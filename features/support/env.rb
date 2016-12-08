b91dce0cefa46cdf2c87ede63b128eac5b4252e3
Nick Rowlands <nick@rowlando.com>
2016-12-08 16:41:41 +0000

    Mock out authenticate! method in cucumber tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -11,3 +11,10 @@ ActiveRecord::Base.logger = nil # Get rid of Sinatra's noisy logging in tests
 
 Dir["#{__dir__}/../../support/factories/*.rb"].each { |f| require f }
 World(FactoryGirl::Syntax::Methods)
+
+module Admin
+  module Helpers
+    def authenticate!
+    end
+  end
+end
