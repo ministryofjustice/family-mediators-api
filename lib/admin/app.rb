e3f80b33e1867f7cdb50a80b27bc8e4f8d4c4fe1
Nick Rowlands <nick@rowlando.com>
2016-12-08 13:35:11 +0000

    wip

--- lib/admin/app.rb
+++ lib/admin/app.rb
@@ -3,32 +3,51 @@ require 'sinatra/json'
 module Admin
   class App < Sinatra::Base
 
-    use Rack::Auth::Basic do |username, password|
-      username == 'admin' and password == 'admin'
-    end
+    # use Rack::Auth::Basic do |username, password|
+    #   username == 'admin' and password == 'admin'
+    # end
 
-    include Helpers
+    TEN_MINUTES   = 60 * 10
+    use Rack::Session::Pool, expire_after: TEN_MINUTES # Expire sessions after ten minutes of inactivity
+    helpers Helpers
 
     set :views, File.dirname(__FILE__) + '/../../views'
     set :public_folder, 'public'
+
+    get '/login' do
+      slim :login
+    end
+
+    post '/login' do
+      if user = User.authenticate(params)
+        session[:user] = user
+        redirect_to_original_request
+      else
+        redirect url('/login?incorrect=true')
+      end
+    end
 
     get '/' do
       slim :start
     end
 
     get '/actions' do
+      authenticate!
       slim :actions
     end
 
     get '/upload' do
+      authenticate!
       slim :index
     end
 
     get '/healthcheck' do
+      authenticate!
       json :status => 'OKAY'
     end
 
     post '/upload' do
+      authenticate!
       begin
         success, details = Services::ProcessFile.new(
           params[:spreadsheet_file]
@@ -47,6 +66,7 @@ module Admin
     end
 
     post '/upload-process' do
+      authenticate!
       success, details = Services::ProcessData.new(params[:dump]).call
 
       if success
@@ -57,10 +77,12 @@ module Admin
     end
 
     get '/upload-success' do
+      authenticate!
       slim :upload_success
     end
 
     get '/upload-fail' do
+      authenticate!
       slim :upload_fail
     end
   end
