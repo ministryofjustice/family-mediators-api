16bba7f1128925cee09a236584d23c7b94bf6dd0
Nick Rowlands <nick@rowlando.com>
2016-12-14 15:35:15 +0000

    Check for null values in cells
    Some blank cells in OOXML file formats return an empty string, while others blank cells return null. See http://blog.softartisans.com/2013/12/30/kb-why-do-some-blank-cells-return-different-values-in-ooxml/

--- spec/admin/parsers/workbook_spec.rb
+++ spec/admin/parsers/workbook_spec.rb
@@ -2,27 +2,48 @@ module Admin
   module Parsers
     describe Workbook do
 
-      let(:rubyxl_workbook) do
-        headings = ['First Name', 'Last Name']
-        data = [%w{ John Smith }, %w{ Donna Jones }]
-        Support::Factories::Spreadsheet.build(headings, data)
-      end
-
-      let(:expected_data) do
-        [
-          {:first_name => 'John', :last_name => 'Smith'},
-          {:first_name => 'Donna', :last_name => 'Jones'}
-        ]
-      end
-
       subject do
         Workbook.new(rubyxl_workbook)
       end
 
       context '#transform_worksheet' do
+
+        let(:rubyxl_workbook) do
+          headings = ['First Name', 'Last Name']
+          data = [%w{ John Smith }, %w{ Donna Jones }]
+          Support::Factories::Spreadsheet.build(headings, data)
+        end
+
+        let(:expected_data) do
+          [
+              {:first_name => 'John', :last_name => 'Smith'},
+              {:first_name => 'Donna', :last_name => 'Jones'}
+          ]
+        end
+
         it 'Transforms data' do
           expect(subject.send(:transform_worksheet)).to eq(expected_data)
         end
+
+        context 'with null values for blank cells' do
+          let(:rubyxl_workbook) do
+            headings = ['First Name', 'Last Name']
+            data = [%w{ John Smith }, [nil, 'Baker'], %w{ Donna Jones }]
+            Support::Factories::Spreadsheet.build(headings, data)
+          end
+          let(:expected_data) do
+            [
+                {:first_name => 'John', :last_name => 'Smith'},
+                {:first_name => '', :last_name => 'Baker'},
+                {:first_name => 'Donna', :last_name => 'Jones'}
+            ]
+          end
+
+          it 'Transforms data' do
+            expect(subject.send(:transform_worksheet)).to eq(expected_data)
+          end
+        end
+
       end
 
       context 'empty workbook' do
@@ -38,6 +59,8 @@ module Admin
           end
         end
       end
+
+
 
     end
   end
