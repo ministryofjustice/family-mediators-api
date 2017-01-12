26c8aee6118772c36f60e79955a3eb6b6956ca3a
Andy White <andy.white@digital.justice.gov.uk>
2017-01-12 09:17:12 +0000

    Use env var DATABASE_URL if RACK_ENV is production

--- lib/mediators.rb
+++ lib/mediators.rb
@@ -3,5 +3,11 @@ LOGGER.level = ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].to_sym : Logger::DEBUG
 LOGGER.info "LOG_LEVEL: #{LOGGER.level}"
 
 db_config = YAML::load_file(File.expand_path('../../config/database.yml', __FILE__))
+
+if ENV['RACK_ENV'] == 'production' && ENV['DATABASE_URL']
+  db_config['production'] = {'url' => ENV['DATABASE_URL']}
+end
+
+puts ">>>>> #{db_config}"
 ActiveRecord::Base.configurations = db_config
 ActiveRecord::Base.establish_connection(ENV['RACK_ENV'].to_sym)
