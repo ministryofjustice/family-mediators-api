39c8b12062c10ac84afc742ce8cb1858d53d5e9c
Lukasz Raczylo <lukasz.raczylo@digital.justice.gov.uk>
2017-01-12 11:42:34 +0000

    Additional actions for database setup

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 cd /usr/src/app
+case ${DOCKER_STATE} in
+create)
+    echo "running create"
+    bundle exec rake db:setup db:seed
+    ;;
+migrate_and_seed)
+    echo "running migrate and seed"
+    bundle exec rake db:migrate db:seed
+    ;;
+esac
 echo "Running app"
 bundle exec rackup
