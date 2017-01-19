b51a7e49a86da7e71750a9a72ef68018d8437125
Nick Rowlands <nick@rowlando.com>
2017-01-19 13:01:13 +0000

    Change telephone reg ex to look for a minimum of 8 numbers. - This was changed because some addresses start with 5 numbers

--- lib/admin/parsers/practice.rb
+++ lib/admin/parsers/practice.rb
@@ -4,7 +4,7 @@ module Admin
       RECORD_SEPARATOR = "\n"
       PART_SEPARATOR = '|'
       EMAIL_REGEX = /@/i
-      TEL_REGEX = /[\d\s-]{5,12}/
+      TEL_REGEX = /[\d\s]{8,12}/
       POSTCODE_REGEX = /((GIR\s*0AA)|((([A-PR-UWYZ][0-9]{1,2})|(([A-PR-UWYZ][A-HK-Y][0-9]{1,2})|(([A-PR-UWYZ][0-9][A-HJKSTUW])|([A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRVWXY]))))\s*[A-Z]?[0-9][ABD-HJLNP-UW-Z]{2}))/i
       URL_REGEX = /\A((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?\Z/i
 
