036eb9de204ca2a60924b31dd603043fd4e62d84
Jesus Laiz <zheileman@users.noreply.github.com>
2019-04-03 13:01:37 +0100

    Bump version of Ruby
    Wheezy and Jessie (except LTS) have been removed from mirrors, so our Dockerfile doesn't work anymore as the Ruby image we were using is too old.
    
    https://lists.debian.org/debian-devel-announce/2019/03/msg00006.html
    
    Bump the version of ruby to 2.5.3 which is conservative enough and should not break anything in this, otherwise, old app, and at the same time add a `docker-compose` file as we did with other service to simplify testing locally.
    
    The bump in ruby version required also a bump in Rubocop and fix the config file to work with the new version.
    
    Prepare the Dockerfile to be able to deploy to Live-1 cluster.

--- lib/mediators.rb
+++ lib/mediators.rb
@@ -4,9 +4,11 @@ LOGGER.info "LOG_LEVEL: #{LOGGER.level}"
 
 db_config = YAML::load_file(File.expand_path('../../config/database.yml', __FILE__))
 
+# :nocov:
 if %w{ staging production }.include?(ENV['RACK_ENV'])
   db_config[ENV['RACK_ENV']] = {'url' => ENV['DATABASE_URL']}
 end
+# :nocov:
 
 ActiveRecord::Base.configurations = db_config
 ActiveRecord::Base.establish_connection(ENV['RACK_ENV'].to_sym)
