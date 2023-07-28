b51509dfb6a2c07076f7edc91f4bb3bd065ae569
N.Preddy <nick.preddy@digital.justice.gov.uk>
2023-07-28 12:21:20 +0100

    Use ApplicationRecord.

--- lib/api/models/mediator.rb
+++ lib/api/models/mediator.rb
@@ -2,7 +2,7 @@ require "active_record"
 
 module API
   module Models
-    class Mediator < ActiveRecord::Base # rubocop:disable Rails/ApplicationRecord
+    class Mediator < ApplicationRecord
       before_create :set_urn_prefix
 
       def set_urn_prefix
