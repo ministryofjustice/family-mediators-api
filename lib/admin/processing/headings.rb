a70223432fccdff65ed51cfaaa48abdf6fa2dbd3
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-09-06 17:27:58 +0100

    Use call

--- lib/admin/processing/headings.rb
+++ lib/admin/processing/headings.rb
@@ -1,7 +1,7 @@
 module Admin
   module Processing
     class Headings
-      def self.process(headings)
+      def self.call(headings)
         headings.map! { |heading|
           heading ? heading.downcase.gsub(/[^a-z0-9]/, "_").squeeze("_").to_sym : nil
         }.compact
--- spec/admin/processing/headings_spec.rb
+++ spec/admin/processing/headings_spec.rb
@@ -2,24 +2,24 @@ module Admin
   module Processing
     describe Headings do
       it "Replaces spaces with underscores" do
-        expect(described_class.process(["foo bar"])).to eq [:foo_bar]
+        expect(described_class.call(["foo bar"])).to eq [:foo_bar]
       end
 
       it "Downcases" do
-        expect(described_class.process(%w[FooBar])).to eq [:foobar]
+        expect(described_class.call(%w[FooBar])).to eq [:foobar]
       end
 
       it "Replaces slashes with underscores" do
-        expect(described_class.process(["foo/bar"])).to eq [:foo_bar]
+        expect(described_class.call(["foo/bar"])).to eq [:foo_bar]
       end
 
       it "Replaces multiple non alpha chars with ONE underscore" do
-        expect(described_class.process(["foo@@@£££££bar"])).to eq [:foo_bar]
-        expect(described_class.process(["foo     bar"])).to eq [:foo_bar]
+        expect(described_class.call(["foo@@@£££££bar"])).to eq [:foo_bar]
+        expect(described_class.call(["foo     bar"])).to eq [:foo_bar]
       end
 
       it "Handles an array correctly" do
-        expect(described_class.process(%w[foo bar])).to eq %i[foo bar]
+        expect(described_class.call(%w[foo bar])).to eq %i[foo bar]
       end
     end
   end
