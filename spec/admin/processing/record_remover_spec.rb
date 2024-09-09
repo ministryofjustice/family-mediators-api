b5d967c2eccbd94b3942e2c0d56586d207a0a4b2
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-09-09 10:08:37 +0100

    New test

--- spec/admin/processing/record_remover_spec.rb
+++ spec/admin/processing/record_remover_spec.rb
@@ -4,12 +4,22 @@ module Admin
       subject(:remover) { described_class.new(records) }
 
       describe "when there are blank records" do
-        let(:records) { [record_one, record_two] }
-        let(:record_one) { { first_name: "", last_name: "", urn: nil } }
-        let(:record_two) { { first_name: "John", last_name: "Smith", urn: "1234A" } }
+        let(:records) { [record_blanks, record_normal] }
+        let(:record_blanks) { { first_name: "", last_name: "", urn: nil } }
+        let(:record_normal) { { first_name: "John", last_name: "Smith", urn: "1234A" } }
 
         it "Removes blank fields" do
-          expect(remover.call).to eq([record_two])
+          expect(remover.call).to eq([record_normal])
+        end
+      end
+
+      describe "when there are student records" do
+        let(:records) { [record_certified, record_student] }
+        let(:record_certified) { { first_name: "Jane", last_name: "Jones", urn: "1234A" } }
+        let(:record_student) { { first_name: "John", last_name: "Smith", urn: "1234S" } }
+
+        it "Removes students" do
+          expect(remover.call).to eq([record_certified])
         end
       end
     end
