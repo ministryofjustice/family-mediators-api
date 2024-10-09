ff2822c767e80d2b47286bc25e9d58842f471510
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-10-09 14:00:21 +0100

    Add govuk_app_config

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,2 @@
-threads_count = ENV.fetch("PUMA_MAX_THREADS", 5).to_i
-threads threads_count, threads_count
-
-port        ENV.fetch("PUMA_PORT", 9292)
-environment ENV.fetch("RACK_ENV") { "development" }
-
-log_requests true
+require "govuk_app_config/govuk_puma"
+GovukPuma.configure_rails(self)
