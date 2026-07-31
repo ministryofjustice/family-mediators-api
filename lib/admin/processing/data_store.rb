83faef85bb1b6e104755d86587c7e930c2540ada
Tony Mckernan <anthony.mckernan@justice.gov.uk>
2026-07-31 17:01:47 +0100

    CDPTKAN-1263 Show record count on upload success page and log to console

--- lib/admin/processing/data_store.rb
+++ lib/admin/processing/data_store.rb
@@ -2,10 +2,12 @@ module Admin
   module Processing
     class DataStore
       def self.save(hashes)
-        ActiveRecord::Base.transaction do
+        count = ActiveRecord::Base.transaction do
           API::Models::Mediator.delete_all
-          API::Models::Mediator.create(with_data_attributes(hashes))
+          API::Models::Mediator.create(with_data_attributes(hashes)).size
         end
+        LOGGER.info "Upload complete: #{count} mediator records written to database"
+        count
       end
 
       # Each record has a 'data' attribute which stores the spreadsheet
